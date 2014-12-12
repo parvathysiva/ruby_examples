f7c9c91f70706720a6570da3adc633c8139dfdc1
Parvathy Sivasamy <parvathysiva18@gmail.com>
2014-12-11 20:44:06 -0800

    1) 12112014/example_if.rb -- example of if 2)  12112014/example_addition.rb -- example of add

---
+++ 12112014/example_addition.rb
@@ -0,0 +1,10 @@
+puts "Enter 1st  value"
+a = gets
+puts "Enter 2nd value"
+b = gets
+puts "Sum is = #{a.to_i + b.to_i}"
+puts "Diff is = #{a.to_i - b.to_i}"
+puts "Mul is = #{a.to_i * b.to_i}"
+puts "Div is = #{a.to_f / b.to_f}"
+
+
---
+++ 12112014/example_if.rb
@@ -0,0 +1,10 @@
+puts "please enter a value"
+a = gets
+if a.to_i > 10
+    puts "value greater than 10"
+elsif a.to_i < 10
+    puts "value less than 10"
+else
+    puts "value equal to 10"
+end
+ 
